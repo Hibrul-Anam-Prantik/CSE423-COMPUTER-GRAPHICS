1f4649f42952c0b3fc4dacc0674c9ac0472f53dd
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2026-03-03 03:40:19 +0600

    house drawing done

---
+++ House_in_Rainfall.py
@@ -0,0 +1,79 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def drawLines(x, y):
+    glBegin(GL_LINES)
+    glVertex2f(x, y)
+    glEnd()
+
+def drawTriangle(x1, y1, x2, y2, x3, y3):
+    glBegin(GL_TRIANGLES)
+    glVertex2f(x1, y1)
+    glVertex2f(x2, y2)
+    glVertex2f(x3, y3)
+    glEnd()
+
+def drawHouse():
+    # house body
+    glColor3f(1.0, 0.98, 0.94)
+    drawTriangle(187.5, 255, 187.5, 190, 312.5, 190)
+    drawTriangle(312.5, 190, 312.5, 255, 187.5, 255)
+    # roof
+    glColor3f(0.6, 0.2, 0.8) 
+    drawTriangle(170, 255, 330, 255, 250, 310)
+    # door
+    glColor3f(0.53, 0.81, 0.98)
+    drawTriangle(235, 190, 265, 190, 265, 230)
+    drawTriangle(235, 190, 235, 230, 265, 230)
+    # Window -> left
+    drawTriangle(200, 215, 225, 215, 225, 240)
+    drawTriangle(200, 215, 200, 240, 225, 240)
+    # Window -> right
+    drawTriangle(275, 215, 300, 215, 300, 240)
+    drawTriangle(275, 215, 275, 240, 300, 240)
+    # door lock "."
+    glColor3f(0, 0, 0)
+    glPointSize(3)
+    glBegin(GL_POINTS)
+    glVertex2f(257, 213)
+    glEnd()
+    # window frames "+"
+    glLineWidth(1)
+    glBegin(GL_LINES)
+    glVertex2f(212.5, 215)
+    glVertex2f(212.5, 240)
+    glVertex2f(200, 227.5)
+    glVertex2f(225, 227.5)
+    glVertex2f(287.5, 215)
+    glVertex2f(287.5, 240)
+    glVertex2f(275, 227.5)
+    glVertex2f(300, 227.5)
+    glEnd()
+
+def setupProjection():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, 500, 0, 500, 0, 1)
+    glMatrixMode(GL_MODELVIEW)
+  
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    setupProjection()
+    drawHouse()
+    
+    glutSwapBuffers()
+    
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(450, 200)
+    glutCreateWindow(b"A House in Rainfall")
+    glutDisplayFunc(display)
+    glutMainLoop()
+
+if __name__ == "__main__":
+    main()
