d139e508af9058ee0066100b050be84091024bea
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2026-04-16 00:56:34 +0600

    Add shape rendering to showScreen

--- Lab03/3D_OpenGL_Intro.py
+++ Lab03/3D_OpenGL_Intro.py
@@ -123,10 +123,16 @@ def showScreen():
     # display game info text at a specific screen position
     draw_text(10, 680, f"A Random Fixed Position Text")
     draw_text(10, 640, f"See how the posotion and variable change?: {rand_var}")
+    
+    draw_shapes()
+    
 
 def mouseListener():
-    
-        
+
+def specialKeyListener():
+       
+def keyboardListener():
+           
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH) # enable double buffering, RGB color mode, and depth testing
